f2bce7f845db02bf865e02a36347081e9618c268
Patrick McGuire <insidenothing@gmail.com>
2015-01-21 12:28:39 -0500

    Update photo.php

--- photo.php
+++ photo.php
@@ -3,7 +3,7 @@ $now = time();
 echo '<pre>';
 // -n no preview
 // -o output file
-$cmd = "raspistill -md 7 -o  $now.jpg";
+$cmd = "raspistill -md 7 -o  $now.jpg &";
 // Outputs all the result of shellcommand "ls", and returns
 // the last output line into $last_line. Stores the return value
 // of the shell command in $retval.
