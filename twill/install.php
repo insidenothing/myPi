60d4ad898f2b528fb1bf8c5b1b781616c6b57254
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 13:54:07 -0500

    Update install.php

--- twill/install.php
+++ twill/install.php
@@ -1,6 +1,16 @@
 <?PHP
 
 $installDir = '/opt/twill';
-
+$file = "/twill-0.9.tar.gz";
+$command = "wget -P $installDir http://darcs.idyll.org/~t/projects$file";
+if (!file_exists($installDir.$file)){
+  exec($command);
+  echo "<li>$command Executed</li>";
+  if (file_exists($installDir.$file)){
+    echo "<li>File Downloaded</li>";
+  }
+}else{
+  echo "<li>Already Downloded</li>";
+}
 
 ?>
