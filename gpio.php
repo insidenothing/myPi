d84b574848996a05e77eda478c5cfdf57558afab
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 11:00:35 -0500

    Create gpio.php

---
+++ gpio.php
@@ -0,0 +1,39 @@
+<?PHP
+// add security here
+
+
+function pin_out($pin,$power){
+  $cmd = "/root/wiringPi/gpio -g write $pin $power";
+  $last_line = system($cmd, $retval);
+}
+
+
+$pin = 17;
+$on = 1;
+$off = 0;
+$i = 0;
+
+
+$init = "gpio -g mode $pin out";
+$last_line = system($init, $retval);
+while ($i < 10){
+  $i++
+  $power= (i % 2 == 0) ? '0' : '1';
+  pin_out($pin,$power);
+}
+
+
+
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('/root/wiringPi/gpio readall', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
+?>
