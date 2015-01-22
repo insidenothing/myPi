5259ae023857fc9b4dcdadb3534459601dc11a1e
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 11:52:24 -0500

    Update camera.php

--- camera.php
+++ camera.php
@@ -1,16 +1,14 @@
 <?php
 if(isset($_GET['still'])){
-ob_start();
+
 // -n no preview
 // -o output file
 $cmd = "raspistill -md 7 -o  cam.jpg";
-// Outputs all the result of shellcommand "ls", and returns
-// the last output line into $last_line. Stores the return value
-// of the shell command in $retval.
+
 $last_line = system($cmd, $retval);
 
-// Printing additional info
-$debug = ob_get_clean();
+
+
 }
 
 ?>
