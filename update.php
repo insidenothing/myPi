824ff1a4287a90bc51d571cad70dbda03bd7a07b
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 11:46:47 -0500

    Update update.php

--- update.php
+++ update.php
@@ -3,7 +3,7 @@ echo '<pre>';
 // Outputs all the result of shellcommand "ls", and returns
 // the last output line into $last_line. Stores the return value
 // of the shell command in $retval.
-$last_line = system('/usr/bin/sudo /root/wiringPi/gpio/gpio readall', $retval);
+$last_line = system('/root/pull', $retval);
 // Printing additional info
 echo '</pre>';
 ?>
