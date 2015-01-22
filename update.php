3524b762a7f5a4f7c661e30500eb0183b346f115
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 11:49:04 -0500

    Delete update.php

--- update.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-echo '<pre>';
-// Outputs all the result of shellcommand "ls", and returns
-// the last output line into $last_line. Stores the return value
-// of the shell command in $retval.
-$last_line = system('/root/pull', $retval);
-// Printing additional info
-echo '</pre>';
-?>
