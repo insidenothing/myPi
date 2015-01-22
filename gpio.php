12590451587240af0ccfd26aeeb551c09df66473
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 11:32:40 -0500

    Update gpio.php

--- gpio.php
+++ gpio.php
@@ -1,29 +1,6 @@
 <?PHP
 // add security here
 
-
-function pin_out($pin,$power){
-  $cmd = "/usr/bin/sudo /root/wiringPi/gpio/gpio -g write $pin $power";
-  $last_line = system($cmd, $retval);
-}
-
-
-$pin = 17;
-$on = 1;
-$off = 0;
-$x = 0;
-
-
-$init = "/usr/bin/sudo /root/wiringPi/gpio/gpio -g mode $pin out";
-$last_line = system($init, $retval);
-while ($x < 3){
-  $x++;
-  $power= ($x % 2 == 0) ? '0' : '1';
-  pin_out($pin,$power);
-}
-
-
-
 echo '<pre>';
 
 // Outputs all the result of shellcommand "ls", and returns
@@ -36,4 +13,30 @@ echo '
 </pre>
 <hr />Last line of the output: ' . $last_line . '
 <hr />Return value: ' . $retval;
+
+function pin_out($pin,$power){
+  $cmd = "/usr/bin/sudo /root/wiringPi/gpio/gpio -g write $pin $power";
+  $last_line = system($cmd, $retval);
+}
+
+
+$pin = 17;
+$on = 1;
+$off = 0;
+$x = 0;
+
+if (isset($_GET['blink'])){
+  $pin = 17;
+  $init = "/usr/bin/sudo /root/wiringPi/gpio/gpio -g mode $pin out";
+  $last_line = system($init, $retval);
+  while ($x < 50){
+    $x++;
+    $power= ($x % 2 == 0) ? '0' : '1';
+    pin_out($pin,$power);
+  }
+  header('Location: gpio.php');
+}
+
+
 ?>
+
