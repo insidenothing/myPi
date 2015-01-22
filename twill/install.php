d9d22f25af02a4f3c3db3174822ce1ca425ebcc6
Patrick McGuire <insidenothing@gmail.com>
2015-01-22 14:03:07 -0500

    Update install.php

--- twill/install.php
+++ twill/install.php
@@ -28,8 +28,19 @@ if (!file_exists($installDir.$file)){
 
 
 $uncompress = "tar -C $installDir xvfz $installDir.$file";
-exec($uncompress);
 
 
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system($uncompress, $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
 
 ?>
